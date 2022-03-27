41040c0c0b9e3bd3d5723364047e302a20016abe
Tucker K <tuckerkloetzke@gmail.com>
2022-03-27 13:02:15 -0500

    Use graphql endpoint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 
 function App() {
+  const ALL_BOOKS = gql`
+    query {
+      getBookshelf {
+        isbn
+        title
+        amazonAverageRating
+        amazonRatingsCount
+        goodreadsAverageRating
+        goodreadsRatingsCount
+        owned
+        price
+        subtitle
+        thumbnail
+        unread
+      }
+    }
+  `
+
+  const { loading, error, data } = useQuery(ALL_BOOKS);
+  console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {data?.getBookshelf.map((book, i) => <li key={i}>{JSON.stringify(book)}</li>)}
+        </ul>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
